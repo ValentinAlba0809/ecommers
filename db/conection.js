6175e204866a35e713c4d090ab27d7f31ed351c4
ValentinAlba0809 <valentinalba0809@gmail.com>
2024-12-13 09:26:49 -0300

    Add files via upload

---
+++ db/conection.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2');
+
+const dbConfig = {
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'Tina45989326',
+    database: 'dbtp5'
+   
+};
+
+const conexion = mysql.createConnection(dbConfig);
+
+conexion.connect(function(err) {
+    if (err) {
+        throw err;
+    } else {
+        console.log('Conexión exitosa');
+    }
+});
+
+module.exports = conexion;
